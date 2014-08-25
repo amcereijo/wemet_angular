e53aeb6254597ecc838f87464ae63d9618f59c39
amcereijo <amcereijo@gmail.com>
2014-08-25 18:31:00 +0200

    showing some igo data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,12 @@
 var controllers = angular.module('wemet.controllers', []);
 
 controllers.controller('IndexCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-	console.log('$rootScope.serviceUrl' + $rootScope.serviceUrl);
+	$rootScope.user = '666666666';
 }]);
 
-controllers.controller('IgoCtrl', ['$scope', '$http', '$rootScope', '$log', function($scope, $http, $rootScope, $log) {
-	$scope.igos = ['one', 'two'];
-	$log.message ='calling ' + $rootScope.serviceUrl+'/igo';
+controllers.controller('IgoCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+	$scope.igos = [];
+	console.log('calling ' + $rootScope.serviceUrl+'/igo');
 	$http({method: 'GET', isArray: true, url: $rootScope.serviceUrl+'/igo'}).
 		success(function(data, status, headers, config) {
 			$scope.igos = data;
@@ -18,4 +18,14 @@ controllers.controller('IgoCtrl', ['$scope', '$http', '$rootScope', '$log', func
 		error(function(data, status, headers, config) {
 			alert('error: '+data);
 		});
+	$scope.getDate = function(date) {
+		var datetime = new Date(date);
+		//TODO change date formatw
+		return datetime.getDate() + '/' + (datetime.getMonth()+1) +
+			'/' + datetime.getFullYear();
+	}
+	$scope.getTime = function(date) {
+		var datetime = new Date(date);
+		return datetime.getHours() + ':' + datetime.getMinutes();
+	}
 }]);
