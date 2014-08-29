7d94507d1ff7bdaf2c9c4322385ce4127fb9122e
amcereijo <amcereijo@gmail.com>
2014-08-29 03:11:25 +0200

    karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,10 +5,13 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
+      'test/unit/controllersSpec.js'
     ],
 
     autoWatch : true,
