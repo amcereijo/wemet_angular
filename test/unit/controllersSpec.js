03979f5020d7491f05102a45cc80a8d2aadda0ad
amcereijo <amcereijo@gmail.com>
2014-09-08 23:39:49 +0200

    adding tets case to controllers test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,19 +2,41 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+ddescribe('controllers', function(){
 
+  beforeEach(module('wemet'));//,['ngResource']));
+  //beforeEach(module('wemet.controllers', ['wemet.services']));
+  
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
-  }));
+  describe('IndexCtrl ', function() {
+    it('should be defined', inject(function($controller) {
+      //spec body
+      var myCtrl1 = $controller('IndexCtrl', { $scope: {} });
+      expect(myCtrl1).toBeDefined();
+    }));
+  });
+  
+  describe('IgoCtrl ', function() {
+    it('should be defined', inject(function($controller) {
+      var myCtrl1 = $controller('IgoCtrl', { $scope: {} });
+      expect(myCtrl1).toBeDefined();
+    }));
+    it('has method getDate', inject(function($controller) {
+      var scope = {},
+        myCtrl1 = $controller('IgoCtrl', { $scope: scope });
+      expect(scope.getDate).toBeDefined();
+    }));
+    it('has method getTime', inject(function($controller) {
+      var scope = {},
+        myCtrl1 = $controller('IgoCtrl', { $scope: scope });
+      expect(scope.getTime).toBeDefined();
+    }));
+    it('has method setResp', inject(function($controller) {
+      var scope = {},
+        myCtrl1 = $controller('IgoCtrl', { $scope: scope });
+      expect(scope.setResp).toBeDefined();
+    }));
+  });
+  
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-    expect(myCtrl2).toBeDefined();
-  }));
 });
